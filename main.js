76d13294ad4f0a246e0072129a17560e633f21e4
Angelino Eurico <angelinoeurico574@gmail.com>
2025-05-26 22:10:26 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,32 +1,31 @@
-// add menu button 
+// add menu 
 
-let button_Menu = document.querySelector('.button-menu')
+let button_menu = document.querySelector('.button-menu')
 
-button_Menu.addEventListener('click', ()=>{
-  let div_Navbar = document.querySelector('.div-navbar')
-  
-  div_Navbar.classList.add('activo-Navbar')
+button_menu.addEventListener('click' , ()=>{
+ 
+ let div_navbar = document.querySelector('.div-navbar')
+ 
+ div_navbar.classList.add('div-navbar-acivo')
+ 
 })
 
-// button-WatsAp remove
+// remove menu
 
+let button_remove = document.querySelector('.button-remove')
 
-let remoVe = document.querySelector('.remove')
+button_remove.addEventListener('click' , ()=>{
+ 
+ let div_navbar = document.querySelector('.div-navbar')
 
-remoVe.addEventListener('click' , ()=>{
-  let div_Navbar = document.querySelector('.div-navbar')
-
-   div_Navbar.classList.remove('activo-Navbar')
-  
+   div_navbar.classList.remove('div-navbar-acivo')
+ 
 })
-
-
-// link add link_Activo
 
 let link = document.querySelectorAll('.link')
 
 link.forEach((link_element)=>{
-  link_element.addEventListener('click' , ()=>{
+  link_element.addEventListener('click', ()=>{
     let link_activo = document.querySelector('.link-activo')
     link_activo.classList.remove('link-activo')
     
